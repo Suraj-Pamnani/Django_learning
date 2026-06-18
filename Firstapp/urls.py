5dedf2056fe0a0e0411aa35b88fd31d72aa5f534
Pamnani Suraj Pradeep <190449687+Suraj-Pamnani@users.noreply.github.com>
2026-06-18 14:40:35 +0530

    day 4 commit

--- Firstapp/urls.py
+++ Firstapp/urls.py
@@ -2,11 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage,name='home'),
-    path('home/', views.homepage,name='home'),
-    path('about/', views.about,name='about'),
-    path('contact/', views.contact,name='contact'),
-    path('shop/', views.shop,name='shop'),
-    path('contactprocess/', views.formpageprocess),     
-    
+
+    path('', views.homepage),
+    path('home/', views.homepage),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('shop/', views.shop),
+
+    path('contactprocess/', views.formpageprocess),
+
+    path('saveSession/', views.saveSession),
+    path('getSession/', views.getSessionData),
+    path('removeSession/', views.deleteSession),
+
+    path('login/', views.loginpage),
+    path('loginprocess/', views.loginprocess),
+    path('dashboard/', views.dashboard),
+    path('logout/', views.logout),
+
 ]
--- Firstapp/views.py
+++ Firstapp/views.py
@@ -1,26 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+
 def homepage(request):
-    return render(request,"home.html")
+    return render(request, "home.html")
+
+
 def contact(request):
-    return render(request,"contact.html")
+    return render(request, "contact.html")
+
+
 def about(request):
-    return render(request,"about.html")
+    return render(request, "about.html")
+
+
 def shop(request):
-    return render(request,"shop.html")
+    return render(request, "shop.html")
+
+
 def formpageprocess(request):
-  a = int(request.POST['n1'])
-  b = int(request.POST['n2'])
-  sum = a + b
-  ans = "A is ",a , " and B is ",b, " sum is ",sum
-  d = ""
-  if sum ==100:
-    d = "if triggred"
-  elif sum<100:
-    d = "elif triggred"
-  else :
-    d = "else triggred"
-  return render(request,'ans.html',{'mya':a,'myb':b,'mysum':sum,'myd':d})
+    a = int(request.POST['n1'])
+    b = int(request.POST['n2'])
+
+    total = a + b
+
+    return HttpResponse("Sum = " + str(total))
 
 
+def saveSession(request):
+    request.session['username'] = "Suraj"
+    return HttpResponse("Session Created")
+
+
+def getSessionData(request):
+    if request.session.has_key('username'):
+        return HttpResponse(request.session['username'])
+    else:
+        return HttpResponse("Session Not Present")
+
+
+def deleteSession(request):
+    if request.session.has_key('username'):
+        del request.session['username']
+
+    return HttpResponse("Session Deleted")
+
+
+# LOGIN
+
+def loginpage(request):
+    return render(request, 'login.html')
+
+
+def loginprocess(request):
+
+    txt1 = request.POST['email']
+
+    request.session['myemail'] = txt1
+
+    return redirect(dashboard)
+
+
+def dashboard(request):
+
+    if request.session.has_key('myemail'):
+
+        return render(request, "dashboard.html")
+
+    else:
+
+        return redirect(loginpage)
+
+
+def logout(request):
+
+    if request.session.has_key('myemail'):
+        del request.session['myemail']
+
+    return redirect(loginpage)
